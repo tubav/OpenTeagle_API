c66afe6bb9fdfd868325894900b56d56a24cd0d6
Alex <alex@willner.ws>
2012-05-16 20:45:36 +0200

    cleanup

--- src/test/java/de/tuberlin/av/openteagle/api/TestUserAPI.java
+++ src/test/java/de/tuberlin/av/openteagle/api/TestUserAPI.java
@@ -5,7 +5,7 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestOpenTeagleAPI {
+public class TestUserAPI {
 	private UserAPI openteagle;
 
 	@Before
